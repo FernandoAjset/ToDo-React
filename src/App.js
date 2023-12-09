e248618c026bf5b3b12d99c4b0ecb75bb4655001
eajset <eajset@lucalza.com>
2023-12-08 19:41:49 -0600

    arreglo footer

--- src/App.js
+++ src/App.js
@@ -32,30 +32,35 @@ const todos = [
 function App() {
   return (
     <React.Fragment>
-      <div className="d-block d-sm-flex justify-content-around mt-4">
-        <div className="col-sm-6 col-12 card p-5">
-          <h5 className="card-title text-center mb-3">Lista de ToDo's pendientes</h5>
-          <TodoSearch 
-          />
-          <TodoList>
-            {todos.map((todo) => (
-              <TodoItem
-                key={todo.text}
-                text={todo.text}
-                complete={todo.complete}
-              />
-            ))}
-          </TodoList>
-          <CreateTodoButton />
+      <div className="d-block d-sm-flex justify-content-between mt-4">
+        <div className="container col-sm-7 col-12">
+          <div className="card p-5">
+            <h5 className="card-title text-center mb-3">
+              Lista de ToDo's pendientes
+            </h5>
+            <TodoSearch />
+            <TodoList>
+              {todos.map((todo) => (
+                <TodoItem
+                  key={todo.text}
+                  text={todo.text}
+                  complete={todo.complete}
+                />
+              ))}
+            </TodoList>
+            <CreateTodoButton />
+          </div>
         </div>
-        <div className="col-sm-5 col-12 p-5 card">
-          <TodoCounter
-            completed={todos.filter((t) => t.complete).length}
-            total={todos.length}
-          />
+        <div className="container col-sm-5 col-12">
+          <div className="p-5 card mt-3 mt-sm-0">
+            <TodoCounter
+              completed={todos.filter((t) => t.complete).length}
+              total={todos.length}
+            />
+          </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </React.Fragment>
   );
 }
--- src/Footer.js
+++ src/Footer.js
@@ -1,14 +1,9 @@
 function Footer() {
   return (
-    <div className="container card my-4 mx-0">
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <div className="col-md-4 d-flex align-items-center">
-          <span className="mb-3 mb-md-0 text-body-secondary">
-            © 2023 Create by Edgar Fernando Ajset
-          </span>
-        </div>
-
-        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+    <div className="px-2 my-4 col">
+      <footer className="card d-flex flex-row justify-content-between p-4">
+        <h6>© 2023 Created by Edgar Fernando Ajset</h6>
+        <ul className="nav justify-content-end list-unstyled d-flex">
           <li className="ms-3">
             <a className="text-body-secondary" href="/">
               <svg
