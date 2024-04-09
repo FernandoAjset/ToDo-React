78a9b0e482637a0f75eeb83f7e1e9e5f30d26a61
Ferajset-G3 <fernando_ajset@hotmail.com>
2024-04-09 08:29:30 -0600

    Inicio de localstorage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { TodoItem } from "./TodoItem";
 import { TodoList } from "./TodoList";
 import { TodoSearch } from "./TodoSearch";
 import { fakeTodos } from "./constants/fake_todo_data";
-import "./css/App.css"
+import "./css/App.css";
 
 function App() {
   const [searchValue, setSearchValue] = React.useState("");
@@ -46,30 +46,46 @@ function App() {
     const todoIndex = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].complete = !newTodos[todoIndex].complete;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const onDeleteTodoFunction = (text) => {
     const newTodos = [...todos.filter((todo) => todo.text !== text)];
+    saveTodos(newTodos);
+  };
+
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos ?? []);
+    localStorage.setItem("TODOS_V1", stringifiedTodos);
     setTodos(newTodos);
   };
 
   useEffect(() => {
-    setTodos(fakeTodos);
+    // Obtener 'TODOS_V1' del localStorage, si no existe, se asigna 'fakeTodos'
+    const storedTodos = localStorage.getItem("TODOS_V1");
+    const parsedTodos = JSON.parse(storedTodos ?? []);
+    if (parsedTodos && parsedTodos.length > 0) {
+      setTodos(parsedTodos);
+    } else {
+      setTodos(fakeTodos);
+      localStorage.setItem("TODOS_V1", JSON.stringify(fakeTodos));
+    }
   }, []); // Este efecto se ejecuta sólo una vez, cuando el componente se monta
 
   useEffect(() => {
     setSearchedTodos(todos);
   }, [todos]); // Este efecto se ejecuta cada vez que 'todos' cambia
-  
+
   return (
     <React.Fragment>
       <div className="px-sm-5 container d-flex flex-column justify-content-between">
         <div className="px-0 m-0">
-          <div className="d-sm-flex
+          <div
+            className="d-sm-flex
           app-content
           d-block flex-sm-row flex-column card p-sm-5 p-3 
-          justify-content-between">
+          justify-content-between"
+          >
             <div className="col-sm-7 col-12 col-card">
               <h5 className="card-title text-center mb-3">
                 Lista de ToDo's pendientes
